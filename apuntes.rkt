b9bd518739643b8c18fb54d1dd76e5313ff445a8
Ricardo Ramírez Lepe <ricardo.r.lepe@hotmail.com>
2019-09-07 13:07:22 -0400

    string y numeros

--- apuntes.rkt
+++ apuntes.rkt
@@ -1,2 +1,33 @@
 #lang racket
 ;;; Esto es un comentario en Scheme
+
+;;; Variable tipo string
+"Esto es un string"
+
+;; funciones para los string
+
+(string? "hola"); return t
+(string? 5); return f
+
+;;; Variable tipo numero
+42 ; return 42
+1/2 ; return 1/2
+2.34 ; return 2.34
+
+;; Funciones para los numeros
+
+; Sumar
+(+ 1 2) ; return 3
+; Restar
+(- 3 2) ; return 1
+; Multiplicar
+(* 5 2) ; return 10
+; Dividir
+(/ 4 2) ; return 2
+
+;; Operadores 
++ ; return #<procedure:+>
+- ; return #<procedure:->
+* ; return #<procedure:*>
+/ ; return #<procedure://>
+
